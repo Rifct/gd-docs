eda5626fad66e5ef3f54392d2cef5d99fc2b2540
xparadoxical <43796685+xparadoxical@users.noreply.github.com>
2026-04-24 02:16:27 +0200

    create ParamDesc and param description list
    #56

---
+++ .vitepress/components/ParamDesc.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import postParams from './post-params.mts';
+
+const { name, secret } = defineProps(['name', 'secret']);
+
+let desc = postParams.descriptions[name] || postParams.descriptions['@default'];
+if (name === 'secret') {
+    desc = desc.replace('$0', postParams.formats.secret[secret][0])
+        .replace('$1', postParams.formats.secret[secret][1]);
+}
+</script>
+
+<template>
+    <span v-html="desc"></span>
+</template>
